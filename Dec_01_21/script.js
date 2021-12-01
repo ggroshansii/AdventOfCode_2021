2569657704c27e32b79f43c26e62782cc6c1e7d2
ggroshansii <garthgroshansii@gmail.com>
2021-12-01 16:42:51 -0500

    answered first part of question for 12/1

--- Dec_01_21/script.js
+++ Dec_01_21/script.js
@@ -1,3 +1,5 @@
+const { readFile } = require('fs');
+
 /*
 
 As the submarine drops below the surface of the ocean, it automatically performs a sonar sweep of the nearby sea floor. On a small screen, the sonar sweep report (your puzzle input) appears: each line is a measurement of the sea floor depth as the sweep looks further and further away from the submarine.
@@ -36,3 +38,24 @@ How many measurements are larger than the previous measurement?
 
 */
 
+let depths = [];
+
+let numIncreases = 0;
+
+
+
+readFile('./input.txt', (err, data) => {
+    if (err) throw err;
+    depths = data.toString().split('\n');
+    findIncreases();
+})
+
+function findIncreases() {
+    for (let i = 0; i < depths.length; i++) {
+        // console.log(typeof Number(depths[i]))
+        if (i > 0 && Number(depths[i]) > Number(depths[i-1])) {
+            numIncreases++;
+        }
+    }
+    console.log(numIncreases); // Answer to first part of question
+}
